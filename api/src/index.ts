25a60a67b3cc1b34d5f2157fb367908f3a14b5ff
NaufalNurAdrian <nuradriannaufal@gmail.com>
2025-01-24 22:46:32 +0700

    add controller router middleware

---
+++ api/src/index.ts
@@ -0,0 +1,28 @@
+
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const PORT: number = 8000;
+
+const app: Application = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: [`${process.env.BASE_URL_FE}`],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
+
+app.get("/api", (req: Request, res: Response) => {
+  res.status(200).send("Welcome to my API");
+});
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`server running on -> http://localhost:${PORT}/api`);
+  });
