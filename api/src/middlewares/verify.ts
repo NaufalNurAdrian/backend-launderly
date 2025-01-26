24d7755ed449885c8ce7989d65f93a8fc066f690
NaufalNurAdrian <nuradriannaufal@gmail.com>
2025-01-27 04:26:01 +0700

    create outlet

---
+++ api/src/middlewares/verify.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UserPayload } from "../custom";
+
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    console.log(token);
+    
+    if (!token) throw { message: "Unauthorize!" };
+    
+    const verifiedUser = verify(token, process.env.JWT_KEY!);
+    console.log(verifiedUser);
+
+    req.user = verifiedUser as UserPayload;
+
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+};
