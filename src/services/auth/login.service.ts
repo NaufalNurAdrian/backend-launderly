a667c3691c4eaee64fbbce6aaddcca914dcb0ef7
muunoooo <mnaufal12321@gmail.com>
2025-01-31 09:51:27 +0700

    add user role on token

--- src/services/auth/login.service.ts
+++ src/services/auth/login.service.ts
@@ -32,7 +32,11 @@ export const loginService = async (
       };
 
     // Create JWT token for the customer
-    const payload = { id: customer.id, email: customer.email };
+    const payload = {
+      id: customer.id,
+      email: customer.email,
+      role: customer.role,
+    };
     const token = sign(payload, process.env.JWT_KEY!, { expiresIn: "1d" });
 
     res
