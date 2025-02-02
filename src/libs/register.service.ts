4266cd202b04710256b8bcc2dcda5232b03fe25d
“NaufalNurAdrian” <nuradriannaufal@gmail.com>
2025-02-02 15:41:07 +0700

    fix merge2

--- src/libs/register.service.ts
+++
@@ -1,9 +0,0 @@
-import prisma from "../prisma";
-
-//customer
-export const findCust = async (email: string) => {
-  const userCust = await prisma.user.findFirst({
-    where: { email: email },
-  });
-  return userCust;
-};
